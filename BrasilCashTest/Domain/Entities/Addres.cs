edd50f5d8d36b135345a7619bb4e0202cce4f1d8
Wendel Guimarães <wendel.guimaraes@outlook.com>
2021-11-29 13:06:25 -0300

    definindo variaveis e metodos

--- BrasilCashTest/Domain/Entities/Addres.cs
+++ BrasilCashTest/Domain/Entities/Addres.cs
@@ -1,7 +1,32 @@
-﻿namespace BrasilCashTest.Domain.Entities
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BrasilCashTest.Domain.Entities
 {
     public class Addres
     {
-        public Addres {}
+        public Addres () {} // rest sharp para carregar o endereço da API, tem que ter um construtor vazio
+
+        public Addres(string street, string district, string city, string state) 
+        {
+            Logradouro = street;
+            Bairro = district;
+            Localidade = city;
+            UF = state;
+        }
+   
+        public Guid Id { get; private set; }
+
+        [JsonProperty("street")]
+        public string Logradouro { get; private set; }
+
+        [JsonProperty("district")]
+        public string Bairro { get; private set; }
+
+        [JsonProperty("city")]
+        public string Localidade { get; private set; }
+
+        [JsonProperty("state")]
+        public string UF { get; private set; }
     }
 }
